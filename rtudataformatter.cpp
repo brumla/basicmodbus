ae02cb0581faa5e232c659992d4b56dcec95a318
brumla <zuber@centrum.cz>
2017-07-07 18:42:37 +0200

    First implementation of the RTU (binary) formatter

--- rtudataformatter.cpp
+++ rtudataformatter.cpp
@@ -2,5 +2,54 @@
 
 RTUDataFormatter::RTUDataFormatter()
 {
+    m_dataToBeSend.clear();
+}
 
+IDataFormatter *RTUDataFormatter::address(quint8 addr)
+{
+    m_address = addr;
+    return this;
+}
+
+IDataFormatter *RTUDataFormatter::function(quint8 func)
+{
+    m_function = func;
+    return this;
+}
+
+IDataFormatter *RTUDataFormatter::write(quint8 highByte, quint8 lowByte)
+{
+    m_dataBuffer.append(highByte, lowByte);
+    return this;
+}
+
+IDataFormatter *RTUDataFormatter::write(quint16 word)
+{
+    return write(word >> 8, word & 0x00FF);
+}
+
+IDataFormatter *RTUDataFormatter::readDataCount(quint16 count)
+{
+    m_isReadOperation = true;
+    m_dataCountHigh = count & 0xFF00 >> 8;
+    m_dataCountLow = count & 0x00FF;
+
+    return this;
+}
+
+IDataFormatter *RTUDataFormatter::startAddress(quint8 highByte, quint8 lowByte)
+{
+    m_startAddressHigh = highByte;
+    m_startAddressLow = lowByte;
+    return this;
+}
+
+IDataFormatter *RTUDataFormatter::startAddress(quint16 addr)
+{
+    return startAddress(addr >> 8, addr & 0xFF);
+}
+
+IDataFormatter *RTUDataFormatter::build()
+{
+    return this;
 }
--- rtudataformatter.h
+++ rtudataformatter.h
@@ -1,11 +1,31 @@
 #ifndef RTUDATAFORMATTER_H
 #define RTUDATAFORMATTER_H
 
+#include "idataformatter.h"
 
-class RTUDataFormatter
+class RTUDataFormatter : public IDataFormatter
 {
 public:
     RTUDataFormatter();
+
+    // IDataFormatter interface
+    IDataFormatter *address(quint8 addr);
+    IDataFormatter *function(quint8 func);
+    IDataFormatter *write(quint8 highByte, quint8 lowByte);
+    IDataFormatter *write(quint16 word);
+    IDataFormatter *readDataCount(quint16 count);
+    IDataFormatter *startAddress(quint8 highByte, quint8 lowByte);
+    IDataFormatter *startAddress(quint16 addr);
+    IDataFormatter *build();
+private:
+    quint8              m_address;
+    quint8              m_function;
+    quint16             m_dataCountHigh;
+    quint16             m_dataCountLow;
+    QByteArray          m_dataBuffer;
+    quint16             m_startAddressLow;
+    quint16             m_startAddressHigh;
+    bool                m_isReadOperation = false;
 };
 
 #endif // RTUDATAFORMATTER_H
