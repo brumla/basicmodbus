f9d5c629e3e7546e7b282d0b74d3c4bce7a806de
brumla <zuber@centrum.cz>
2017-07-07 18:03:49 +0200

    The chained methods returns pointer instead of instance (should return this)

--- idataformatter.cpp
+++ idataformatter.cpp
@@ -1,6 +1,1 @@
 #include "idataformatter.h"
-
-IDataFormatter::IDataFormatter()
-{
-
-}
--- idataformatter.h
+++ idataformatter.h
@@ -1,30 +1,30 @@
 #ifndef IDATAFORMATTER_H
 #define IDATAFORMATTER_H
 
+#include <QByteArray>
 #include <QtGlobal>
 
 ///
 /// \brief This is data formatter top abstract class
 ///
+class IDataFormatter;
 class IDataFormatter
 {
 public:
-    IDataFormatter();
-
     /* Encode data */
     ///
     /// \brief add the address
     /// \param addr 1 byte address in range 0..254 (0 is broadcast)
     /// \return current instance of the data formatter
     ///
-    virtual IDataFormatter address(quint8 addr) = 0;
+    virtual IDataFormatter* address(quint8 addr) = 0;
 
     ///
     /// \brief add the function code
     /// \param func function code (1 byte)
     /// \return
     ///
-    virtual IDataFormatter function(quint8 func) = 0;
+    virtual IDataFormatter* function(quint8 func) = 0;
 
     ///
     /// \brief write one word - the word is saved into the buffer
@@ -32,20 +32,20 @@ public:
     /// \param lowByte
     /// \return
     ///
-    virtual IDataFormatter write(quint8 highByte, quint8 lowByte) = 0;
+    virtual IDataFormatter* write(quint8 highByte, quint8 lowByte) = 0;
 
     ///
     /// \brief add the expected bytes which will be read from the bus (only for read operation)
     /// \param count count of the read bytes as word
     /// \return
     ///
-    virtual IDataFormatter readDataCount(quint16 count) = 0;
+    virtual IDataFormatter* readDataCount(quint16 count) = 0;
 
     ///
     /// \brief build the datablock, data are available via dataToBeSend() function
     /// \return
     ///
-    virtual IDataFormatter build() = 0;
+    virtual IDataFormatter* build() = 0;
 
     ///
     /// \brief the data buffer contains complete block of dat to be send including CRC
